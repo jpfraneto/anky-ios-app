93e80e187cf431bde2a0fe347e2c04616127adec
Jorge Pablo Franetovic Stocker <jpfraneto@gmail.com>
2026-03-07 08:23:41 -0300

    Initial Commit

---
+++ AnkyTests/AnkyTests.swift
@@ -0,0 +1,16 @@
+//
+//  AnkyTests.swift
+//  AnkyTests
+//
+//  Created by kithkui on 07-03-26.
+//
+
+import Testing
+
+struct AnkyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
